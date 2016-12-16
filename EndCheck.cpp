949074ac80429e053b73ed7c9896fff99a02f5a0
Kuske <lucky.777yusuke@gmail.com>
2016-12-17 06:52:02 +0900

    EndCheck() 保存選択追加

--- EndCheck.cpp
+++ EndCheck.cpp
@@ -1,13 +1,18 @@
 // include
 #include "DxLib.h"
 #include "EndCheck.h"
+#include "stdio.h"
 
 // 終了確認処理（「はい」が押されたら終了）
-int EndCheck(){
-	switch (MessageBox(GetMainWindowHandle(), _T("終了しますか？"), _T("確認"), MB_YESNOCANCEL))
+int EndCheck() {
+	switch (MessageBox(GetMainWindowHandle(), _T("保存しますか？"), _T("確認"), MB_YESNOCANCEL))
 	{
 	case IDYES:
 		return TRUE;
+		break;
+	case IDNO:
+		DxLib_End();
+		exit(0);
 		break;
 	default:
 		break;
