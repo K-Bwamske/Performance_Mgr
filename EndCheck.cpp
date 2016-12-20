6df20314b7deed70eeeae39bba73645d4a9d7590
Kuske <lucky.777yusuke@gmail.com>
2016-12-20 17:51:01 +0900

    終了関係の微調整

--- EndCheck.cpp
+++ EndCheck.cpp
@@ -4,10 +4,14 @@
 
 // 終了確認処理（「はい」が押されたら終了）
 int EndCheck(){
-	switch (MessageBox(GetMainWindowHandle(), _T("終了しますか？"), _T("確認"), MB_YESNOCANCEL))
+	switch (MessageBox(GetMainWindowHandle(), _T("保存しますか？"), _T("確認"), MB_YESNOCANCEL))
 	{
 	case IDYES:
 		return TRUE;
+		break;
+	case IDNO:
+		DxLib_End();
+		exit(0);
 		break;
 	default:
 		break;
