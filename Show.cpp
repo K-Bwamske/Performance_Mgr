41c30c6a602ea64afde33a53ca652dfee4e59d23
Kuske <lucky.777yusuke@gmail.com>
2016-12-21 19:10:52 +0900

    スクロールバーの移動する向き修正

--- Show.cpp
+++ Show.cpp
@@ -22,8 +22,8 @@ void Show_Init() {
 
 // 更新
 void Show_Update() {
-	if (CheckHitKey(KEY_INPUT_RIGHT) != 0) NowDrawPos_W -= speed;
-	if (CheckHitKey(KEY_INPUT_LEFT) != 0) NowDrawPos_W += speed;
+	if (CheckHitKey(KEY_INPUT_RIGHT) != 0) NowDrawPos_W += speed;
+	if (CheckHitKey(KEY_INPUT_LEFT) != 0) NowDrawPos_W -= speed;
 	if (NowDrawPos_W < 0) NowDrawPos_W = 0;
 	if (scrool_all < NowDrawPos_W) NowDrawPos_W = scrool_all;
 }
@@ -32,13 +32,19 @@ void Show_Update() {
 void Show_Draw() {
 	static int Window_W, Window_H;		// 一応作った（名前は後で変えてね）(WindowSizeMgrに新しい関数を作るのを推奨)
 	GetWindowSize(&Window_W, &Window_H);
-	DrawFormatString(NowDrawPos_W % Window_W, 100, GetColor(0, 0, 0), "%d", std::abs(NowDrawPos_W) / Window_W + 1);
+
+	// 移動していることの確認用(適当に数字を書いてます)
+	DrawFormatString((scrool_all - NowDrawPos_W) % Window_W, 100, GetColor(0, 0, 0), "%d", scrool_all / Window_W + 1 - std::abs(NowDrawPos_W) / Window_W);
+	// スクロールバーの全体を描画
 	DrawBox(0, Window_H - 50, Window_W, Window_H, GetColor(180, 180, 180), TRUE);
 
 	int bar_size;
 	int bar_pos;
 
+	// スクロールバーの情報更新
 	bar_size = (Window_W * Window_W) / scrool_all;
 	bar_pos = (NowDrawPos_W * (Window_W - bar_size)) / scrool_all;
+
+	// スクロールバー（黒）描画
 	DrawBox(bar_pos, Window_H - 50, bar_pos + bar_size, Window_H, GetColor(50, 50, 50), TRUE);
 }
