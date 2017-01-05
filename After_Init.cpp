c0ec8a488ba7442a4fb76f47f7b5cf95f27faeea
Kuske <lucky.777yusuke@gmail.com>
2017-01-05 21:48:14 +0900

    起動時の挙動修正完了！

--- After_Init.cpp
+++ After_Init.cpp
@@ -1,1 +1,9 @@
 #include <DxLib.h>
+
+// DxLib_Init()の後にやる処理
+void After_Init() {
+	SetDrawScreen(DX_SCREEN_BACK);
+	SetWindowUserCloseEnableFlag(FALSE);
+	SetAlwaysRunFlag(TRUE);
+	SetDoubleStartValidFlag(TRUE);				// ウィンドウの多重起動を許可する
+}
